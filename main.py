dd933dc96a77c4b9bc89e484fa9f2b4ec25d4b21
nishatme <omicronicnishat@gmail.com>
2021-09-23 00:46:18 +0600

    added the capability to list the mistaken question numbers after the exam is over

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from ans import make_dict
 from ansk import make_keys
 
+mistakes = list()
 def give_exam():
     num=0
     numpq=float(input("Please tell us how many mark(s) each question would carry...\n"))
-    ded=float(input("Input marks to be deducted for each question answered wrong (if any):\n"))
+    ded=float(input("Please tell us the marks to be deducted for each question answered wrong (if any):\n"))
     answer_keys=make_keys()
     print("#####################################\n\n\n\n")
     answers=make_dict()
@@ -15,6 +16,7 @@ def give_exam():
             if v==" ":
                 num-=0
             else:
+                mistakes.append(i)
                 num-=ded
     return num
 
@@ -22,3 +24,7 @@ def give_exam():
 if __name__=="__main__":
     result=give_exam()
     print("\n====================\nYou have got",result,"numbers!\n=======================\n")
+    if len(mistakes)>=1:
+        print("Your mistakes were the following:\n")
+        for i in range(len(mistakes)):
+            print("Question number:",mistakes[i]);
